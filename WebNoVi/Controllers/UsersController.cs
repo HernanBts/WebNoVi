94029a08ead534c2ad3fe3537447c1ee59c3901c
Hernán Bs <bort05@hotmail.com>
2018-10-30 16:14:46 -0300

    Adds: - Models User, Roles - all functions of superuser permisions manager.

---
+++ WebNoVi/Controllers/UsersController.cs
@@ -0,0 +1,227 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebNoVi.Models;
+using WebNoVi.ModelView;
+
+namespace WebNoVi.Controllers
+{
+    public class UsersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var users = userManager.Users.ToList();
+            var usersView = new List<UserView>();
+
+            foreach (var user in users)
+            {
+                var userView = new UserView
+                {
+                    Email = user.Email,
+                    Name = user.UserName,
+                    UserId = user.Id
+                };
+                usersView.Add(userView);
+            }
+
+            return View(usersView);
+        }
+
+        public ActionResult Roles(string userId)
+        {
+            if (string.IsNullOrEmpty(userId))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var users = userManager.Users.ToList();
+            var user = users.Find(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var roles = roleManager.Roles.ToList();
+            var rolesView = new List<RoleView>();
+
+            foreach (var item in user.Roles)
+            {
+                var role = roles.Find(r => r.Id == item.RoleId);
+
+                var roleView = new RoleView
+                {
+                    RoleId = role.Id,
+                    Name = role.Name
+                };
+                rolesView.Add(roleView);
+            }
+
+            var userView = new UserView
+            {
+                Email = user.Email,
+                Name = user.UserName,
+                UserId = user.Id,
+                Roles = rolesView
+            };
+            return View(userView);
+        }
+
+        public ActionResult AddRole(string userId)
+        {
+            if (string.IsNullOrEmpty(userId))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var users = userManager.Users.ToList();
+            var user = users.Find(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            var userView = new UserView
+            {
+                Email = user.Email,
+                Name = user.UserName,
+                UserId = user.Id
+            };
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var list = roleManager.Roles.ToList();
+            list.Add(new IdentityRole { Id = "", Name = "{Elejir un rol...}" });
+            list = list.OrderBy(r => r.Name).ToList();
+            ViewBag.RoleId = new SelectList(list, "Id", "Name");
+
+            return View(userView);
+        }
+
+        [HttpPost]
+        public ActionResult AddRole(string userId, FormCollection form)
+        {
+            var roleId = Request["RoleId"];
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var users = userManager.Users.ToList();
+            var user = users.Find(u => u.Id == userId);
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var roles = roleManager.Roles.ToList();
+            var role = roles.Find(r => r.Id == roleId);
+
+            var userView = new UserView
+            {
+                Email = user.Email,
+                Name = user.UserName,
+                UserId = user.Id
+            };
+
+            if (string.IsNullOrEmpty(roleId))
+            {
+                ViewBag.Error = "Se debe seleccionar un rol.";
+
+                var list = roleManager.Roles.ToList();
+                list.Add(new IdentityRole { Id = "", Name = "{Elejir un rol...}" });
+                list = list.OrderBy(r => r.Name).ToList();
+                ViewBag.RoleId = new SelectList(list, "Id", "Name");
+
+                return View(userView);
+            }
+
+            if (!userManager.IsInRole(userId, role.Name))
+            {
+                userManager.AddToRole(userId, role.Name);
+            }
+
+            var rolesView = new List<RoleView>();
+
+            foreach (var item in user.Roles)
+            {
+                role = roles.Find(r => r.Id == item.RoleId);
+
+                var roleView = new RoleView
+                {
+                    RoleId = role.Id,
+                    Name = role.Name
+                };
+                rolesView.Add(roleView);
+            }
+
+            userView = new UserView
+            {
+                Email = user.Email,
+                Name = user.UserName,
+                UserId = user.Id,
+                Roles = rolesView
+            };
+            return View("Roles", userView);
+        }
+
+        public ActionResult Delete(string userId, string roleId)
+        {
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(roleId))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var users = userManager.Users.ToList();
+            var user = users.Find(u => u.Id == userId);
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var roles = roleManager.Roles.ToList();
+            var role = roles.Find(r => r.Id == roleId);
+
+            if (userManager.IsInRole(user.Id, role.Name))
+            {
+                userManager.RemoveFromRole(user.Id, role.Name);
+            }
+
+            var rolesView = new List<RoleView>();
+
+            foreach (var item in user.Roles)
+            {
+                role = roles.Find(r => r.Id == item.RoleId);
+
+                var roleView = new RoleView
+                {
+                    RoleId = role.Id,
+                    Name = role.Name
+                };
+                rolesView.Add(roleView);
+            }
+
+            var userView = new UserView
+            {
+                Email = user.Email,
+                Name = user.UserName,
+                UserId = user.Id,
+                Roles = rolesView
+            };
+            return View("Roles", userView);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
